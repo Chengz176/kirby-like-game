5d64749d93707c33bd084a60d9fbcbc95d055653
Chengz176 <70776633+Chengz176@users.noreply.github.com>
2024-09-21 01:16:31 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/kirby-style-game/',
+    base: '/kirby-like-game/',
     plugins: [react()],
     build: {
         minify: false,
